33a42e27c0813fb814a3f3a279c76d0f2e4244d4
idx <idx-horizon@users.noreply.github.com>
2022-01-17 22:22:59 +0000

    fix

--- utils/features.py
+++ utils/features.py
@@ -1,13 +1,13 @@
-import utils.rules 
+import utils.rules as RULE
 features = {
  'adr-119': [
-        {'description': 'victim_age',   'function':  utils.rules.is_over_15, 'params': 'v_age'},
-        {'description': 'offender_age', 'function':  utils.rules.is_over_15, 'params': 'o_age'},
-        {'description': 'relationship', 'function':  utils.rules.is_relationship, 'params': 'relationship'},
-        {'description': 'category_code', 'function': utils.rules.is_category_code, 'params': 'category_code'},
-        {'description': 'victim_age_range', 'function': util.rules.in_range, 'params': 'v_age'}
+        {'description': 'victim_age',   'function':  RULE.is_over_15, 'params': 'v_age'},
+        {'description': 'offender_age', 'function':  RULE.is_over_15, 'params': 'o_age'},
+        {'description': 'relationship', 'function':  RULE.is_relationship, 'params': 'relationship'},
+        {'description': 'category_code', 'function': RULE.is_category_code, 'params': 'category_code'},
+        {'description': 'victim_age_range', 'function': RULE.in_range, 'params': 'v_age'}
   ],
   'adr-160': [
-	{'description': 'category_code', 'function': utils.rules.is_category_code, 'params': 'category_code'}
+	{'description': 'category_code', 'function': RULE.is_category_code, 'params': 'category_code'}
   ]
 }
