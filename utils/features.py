cd1b4712639edc9b520be95c498f57cc1a31980d
idx <idx-horizon@users.noreply.github.com>
2022-01-17 22:20:16 +0000

    in_range rule

--- utils/features.py
+++ utils/features.py
@@ -4,7 +4,8 @@ features = {
         {'description': 'victim_age',   'function':  utils.rules.is_over_15, 'params': 'v_age'},
         {'description': 'offender_age', 'function':  utils.rules.is_over_15, 'params': 'o_age'},
         {'description': 'relationship', 'function':  utils.rules.is_relationship, 'params': 'relationship'},
-        {'description': 'category_code', 'function': utils.rules.is_category_code, 'params': 'category_code'}
+        {'description': 'category_code', 'function': utils.rules.is_category_code, 'params': 'category_code'},
+        {'description': 'victim_age_range' 'function': util.rules.in_range, 'params': 'v_age'}
   ],
   'adr-160': [
 	{'description': 'category_code', 'function': utils.rules.is_category_code, 'params': 'category_code'}
--- utils/rules.py
+++ utils/rules.py
@@ -1,19 +1,16 @@
 import utils.config as config
 
 def is_relationship(param):
-#	valid = ['father','mother', 'partner', 'husband', 'wife']
-	valid = config.valid_relationships
 	if not param:
 		param = ''
 
-	return 1 if param.lower() in valid else 0
+	return 1 if param.lower() in config.valid_relationships else 0
 
 def is_category_code(param):
-	valid =  config.valid_category_codes #['01','02', '03']
 	if not param:
 		param = ''
 
-	return  1 if param.lower() in valid else 0
+	return  1 if param.lower() in config.valid_category_codes else 0
 
 def is_over_15(param):
 	if not param:
@@ -22,5 +19,13 @@ def is_over_15(param):
 		return 1
 	elif param >=0 and param<16:
 		return -5
+	else:
+		return 0
+
+def in_range(param, min_value=0, max_value=15):
+	if not param:
+		return 0
+	if param in range(min_value, max_value):
+		return 1
 	else:
 		return 0
